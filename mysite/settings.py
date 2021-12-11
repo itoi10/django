a14a6ca907668f602cf1d88aae57c8805b77f930
itict10 <itict10@gmail.com>
2021-12-11 20:53:01 +0900

    fix setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_URL = f'{BASE_DIR}/staticfiles/'
+STATIC_ROOT = f'{BASE_DIR}/staticfiles/'
 
 # .envファイル読み込み
 env = environ.Env()
